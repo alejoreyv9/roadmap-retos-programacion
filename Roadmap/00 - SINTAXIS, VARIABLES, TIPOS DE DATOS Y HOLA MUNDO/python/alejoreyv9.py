2f1319735114e896199dd35ac4d9d510d384a132
Alejandro Rey Vera <alejoreyv@gmail.com>
2024-06-03 20:53:09 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/alejoreyv9.py
@@ -0,0 +1,47 @@
+# https://www.python.org/
+
+# Comentario en una Sola linea en python.
+
+"""
+Comentario de python en varias lineas
+que quieras comentar, con comillas dobles.
+
+"""
+'''
+Tambien contamos
+con las comillas simples
+para realizar comentarios.
+
+'''
+
+la_variable = "Esta seria mi variable" # Esto seria para definir una variable. Debe de ser minuscula.
+LA_CONSTANTE = 9.2 # Es la que define un valor unico y que no cambiara en su ejecucion, pero es por convencion, nos hace saber que este valor no deberia de cambiar pero como tal puede cambiar en Python.
+
+
+# - Los tipos de datos primitivos.
+# Strin, cadena de texto.
+tipo_string = "Pepito Perez"
+tipo_string_other = 'Juanito Perez'
+
+# Enteros, numeros enteros positivo o negativo.
+tipo_int = 10
+
+# Flotante, numeros con puntos decimal los reales.
+tipo_float = 3.2315
+
+# Boleanos, bool representan valores verdaderos "True" y "False"
+tipo_booleano_True = True
+tipo_booleano_False = False
+
+# Ningun valor, NoneType. Ausencia de valor.
+tipo_none = None
+
+
+
+# Imprimir por pantalla.
+
+print("¡Hola mundo, Relizo mis ejercicios con  python!")
+
+
+
+
